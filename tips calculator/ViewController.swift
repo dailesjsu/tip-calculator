f42e196a15513c91c87986abbfd8e93b4bd83200
Dai Le <dai.n.le@sjsu.edu>
2019-12-22 16:38:06 -0800

    Initial Commit

---
+++ tips calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tips calculator
+//
+//  Created by Dai Le on 12/22/19.
+//  Copyright © 2019 Dai Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
